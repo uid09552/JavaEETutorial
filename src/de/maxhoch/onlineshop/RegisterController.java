f77e39b6ccbf0f3bf8231adddc1b0cc4141eac17
max.hoch@hotmail.com <max.hoch@hotmail.com>
2015-11-29 00:53:42 +0100

    EnitityManager added

--- src/de/maxhoch/onlineshop/RegisterController.java
+++ src/de/maxhoch/onlineshop/RegisterController.java
@@ -2,9 +2,18 @@ package de.maxhoch.onlineshop;
 
 import java.io.Serializable;
 
+import javax.annotation.Resource;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 
 @Named
 @RequestScoped
@@ -14,6 +23,14 @@ public class RegisterController implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	@PersistenceContext(unitName="onlineshop")
+	private EntityManager em;
+	
+	
+	//Bean Ressource
+	@Resource
+	private UserTransaction ut;
 	
 	
 	@Inject
@@ -31,6 +48,35 @@ public class RegisterController implements Serializable {
 	
 	public String persist()
 	{
+		try {
+			ut.begin();
+			em.persist(customer);
+			try {
+				ut.commit();
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalStateException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (RollbackException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (HeuristicMixedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (HeuristicRollbackException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} catch (NotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SystemException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return "register";
 	}
 	
